b8149d05d8889d83bcd6b74304e430039a1dbd38
john-blaine <john.alan.blaine@gmail.com>
2020-06-14 16:53:05 -0600

    fix syntax error

--- conditional_logic_refresher.py
+++ conditional_logic_refresher.py
@@ -1,6 +1,6 @@
 day_of_week = input('What day of the week is it today? ')
 
-if day_of_week == 'Monday'"
+if day_of_week == 'Monday':
   print('Have a great start to your week!')
 
 print('This always runs.')
