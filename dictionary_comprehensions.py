30d85c09f07a03f1eac8cd470b97d8b787e07f7c
john-blaine <john.alan.blaine@gmail.com>
2020-06-17 14:17:26 -0600

    rudimentary user authentication

--- dictionary_comprehensions.py
+++ dictionary_comprehensions.py
@@ -8,3 +8,14 @@ users = [
 username_mapping = { user[1]: user for user in users }
 
 print(username_mapping)
+
+
+username_input = input('Enter your username: ')
+password_input = input('Enter your password: ')
+
+_, username, password = username_mapping[username_input]
+
+if password_input == password:
+  print('Your details are correct!')
+else:
+  print('Your details are incorrect.')
